581acf0c1cfc5b481f91050580aca81ea4d98cae
RohanBPatil <rohanpatil14218@gmail.com>
2020-11-19 19:45:42 +0530

    Added : Functionality to validate email

--- js/forms.js
+++ js/forms.js
@@ -1,16 +1,26 @@
 const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
-text.addEventListener('input',function(){
+text.addEventListener('input', function () {
     let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-    if(nameRegex.test(text.value)){
+    if (nameRegex.test(text.value)) {
         textError.textContent = "";
-    }else{
+    } else {
         textError.textContent = "Name is incorrect";
     }
-}); 
+});
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
-salary.addEventListener('input', function(){
+salary.addEventListener('input', function () {
     output.textContent = salary.value;
 });
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input', function () {
+    let emailRegex = RegExp('^(abc[.][A-Za-z]+@bl[.]co[.][A-Za-z]{2,})$');
+    if (emailRegex.test(email.value)) {
+        emailError.textContent = "";
+    } else {
+        emailError.textContent = "Email is incorrect";
+    }
+}); 
